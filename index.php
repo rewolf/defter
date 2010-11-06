b298d501f1936ab38d340f95effd6a230d8b31b6
JuzzWuzz <juzz@crause.com>
2010-11-06 18:15:26 +0200

    Worked on some gallery stuff. Added thumbs. Refactored all files to be neater =)

--- index.php
+++ index.php
@@ -1,78 +1,79 @@
 <!doctype html>
 <html>
 <head>
-	<title>Real-Time Deformable Terrain</title>
-	<link rel="stylesheet" href="style.css" type="text/css" media="screen" /> 
+    <title>Real-Time Deformable Terrain</title>
+    
+    <link rel="stylesheet" href="style.css" type="text/css" media="screen" /> 
 </head>
 <body>
 <div id="container">
-  <!------------------------------------------------------------->
-  <!---- BANNER ---->
-  <!------------------------------------------------------------->
-	<header>
-		<h1>BRUCE BANNER</h1>
-	</header>
-  <!------------------------------------------------------------->
-  <!---- NAVIGATION ---->
-  <!------------------------------------------------------------->
-  <nav>
-    <ul>
-      <li><a href="index.php">      Home        </a></li>
-      <li><a href="gallery.php">    Gallery     </a></li>
-      <li><a href="details.php">    Details     </a></li>
-      <li><a href="downloads.php">  Downloads   </a></li>
-      <li><a href="#">              Contact     </a></li>
-    </ul>
-	</nav>
-
-  <!------------------------------------------------------------->
-  <!---- TEASER ---->
-  <!------------------------------------------------------------->
-	<section id="teaser">
+    <!------------------------------------------------------------->
+    <!---- BANNER ---->
+    <!------------------------------------------------------------->
     <header>
+        <h1>BRUCE BANNER</h1>
     </header>
-	</section>
-  
-  <!-- start content -->
-	<div id="content">
-  
     <!------------------------------------------------------------->
-    <!---- MAIN CONTENT ---->
+    <!---- NAVIGATION ---->
     <!------------------------------------------------------------->
-  	<div id="mainContent">
-    	<section id="sectGroup">
-        <section>
-          <header>
-            <h2>Welcome Title</h2>
-            <p>
-              Subtitle
-            </p>
-          </header>
-          <div class="twocolumn">
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin euismod tellus eu orci imperdiet nec rutrum lacus blandit. Cras enim nibh, sodales ultricies elementum vel, fermentum id tellus. Proin metus odio, ultricies eu pharetra dictum, laoreet id odio. Curabitur in odio augue. Morbi congue auctor interdum. Phasellus sit amet metus justo. Phasellus vitae tellus orci, at elementum ipsum. In in quam eget diam adipiscing condimentum. Maecenas gravida diam vitae nisi convallis vulputate quis sit amet nibh. Nullam ut velit tortor. Curabitur ut elit id nisl volutpat consectetur ac ac lorem. Quisque non elit et elit lacinia lobortis nec a velit. Sed ac nisl sed enim consequat porttitor.
-            </p>
-            <p>
-            Pellentesque ut sapien arcu, egestas mollis massa. Fusce lectus leo, fringilla at aliquet sit amet, volutpat non erat. Aenean molestie nibh vitae turpis venenatis vel accumsan nunc tincidunt. Suspendisse id purus vel felis auctor mattis non ac erat. Pellentesque sodales venenatis condimentum. Aliquam sit amet nibh nisi, ut pulvinar est. Sed ullamcorper nisi vel tortor volutpat eleifend. Vestibulum iaculis ullamcorper diam consectetur sagittis. Quisque vitae mauris ut elit semper condimentum varius nec nisl. Nulla tempus porttitor dolor ac eleifend. Proin vitae purus lectus, a hendrerit ipsum. Aliquam mattis lacinia risus in blandit.
-            </p>
-            <p>
-            Vivamus vitae nulla dolor. Suspendisse eu lacinia justo. Vestibulum a felis ante, non aliquam leo. Aliquam eleifend, est viverra semper luctus, metus purus commodo elit, a elementum nisi lectus vel magna. Praesent faucibus leo sit amet arcu vehicula sed facilisis eros aliquet. Etiam sodales, enim sit amet mollis vestibulum, ipsum sapien accumsan lectus, eget ultricies arcu velit ut diam. Aenean fermentum luctus nulla, eu malesuada urna consequat in. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque massa lacus, sodales id facilisis ac, lobortis sed arcu. Donec hendrerit fringilla ligula, ac rutrum nulla bibendum id. Cras sapien ligula, tincidunt eget euismod nec, ultricies eu augue. Nulla vitae velit sollicitudin magna mattis eleifend. Nam enim justo, vulputate vitae pretium ac, rutrum at turpis. Aenean id magna neque. Sed rhoncus aliquet viverra.
-            </p>
-          </div>
-        </section>
-      </section>
-    </div> <!-- end mainContent -->
-      
-      <!------------------------------------------------------------->
-      <!---- SIDEBAR CONTENT ---->
-      <!------------------------------------------------------------->
-      <aside>
-      	
-      </aside>
-  </div> <!-- end content -->
-	<footer>
-		<!-- Footer -->
-	</footer>
+    <nav>
+        <ul>
+            <li><a href="index.php">        Home       </a></li>
+            <li><a href="details.php">      Details    </a></li>
+            <li><a href="gallery.php">      Gallery    </a></li>
+            <li><a href="downloads.php">    Downloads  </a></li>
+            <li><a href="#">                Contact    </a></li>
+        </ul>
+    </nav>
+
+    <!------------------------------------------------------------->
+    <!---- TEASER ---->
+    <!------------------------------------------------------------->
+    <section id="teaser">
+        <header>
+        </header>
+    </section>
+    
+    <!-- start content -->
+    <div id="content">
+    
+        <!------------------------------------------------------------->
+        <!---- MAIN CONTENT ---->
+        <!------------------------------------------------------------->
+        <div id="mainContent">
+            <section id="sectGroup">
+                <section>
+                    <header>
+                        <h2>Welcome Title</h2>
+                        <p>
+                            Subtitle
+                        </p>
+                    </header>
+                    <div class="twocolumn">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin euismod tellus eu orci imperdiet nec rutrum lacus blandit. Cras enim nibh, sodales ultricies elementum vel, fermentum id tellus. Proin metus odio, ultricies eu pharetra dictum, laoreet id odio. Curabitur in odio augue. Morbi congue auctor interdum. Phasellus sit amet metus justo. Phasellus vitae tellus orci, at elementum ipsum. In in quam eget diam adipiscing condimentum. Maecenas gravida diam vitae nisi convallis vulputate quis sit amet nibh. Nullam ut velit tortor. Curabitur ut elit id nisl volutpat consectetur ac ac lorem. Quisque non elit et elit lacinia lobortis nec a velit. Sed ac nisl sed enim consequat porttitor.
+                        </p>
+                        <p>
+                            Pellentesque ut sapien arcu, egestas mollis massa. Fusce lectus leo, fringilla at aliquet sit amet, volutpat non erat. Aenean molestie nibh vitae turpis venenatis vel accumsan nunc tincidunt. Suspendisse id purus vel felis auctor mattis non ac erat. Pellentesque sodales venenatis condimentum. Aliquam sit amet nibh nisi, ut pulvinar est. Sed ullamcorper nisi vel tortor volutpat eleifend. Vestibulum iaculis ullamcorper diam consectetur sagittis. Quisque vitae mauris ut elit semper condimentum varius nec nisl. Nulla tempus porttitor dolor ac eleifend. Proin vitae purus lectus, a hendrerit ipsum. Aliquam mattis lacinia risus in blandit.
+                        </p>
+                        <p>
+                            Vivamus vitae nulla dolor. Suspendisse eu lacinia justo. Vestibulum a felis ante, non aliquam leo. Aliquam eleifend, est viverra semper luctus, metus purus commodo elit, a elementum nisi lectus vel magna. Praesent faucibus leo sit amet arcu vehicula sed facilisis eros aliquet. Etiam sodales, enim sit amet mollis vestibulum, ipsum sapien accumsan lectus, eget ultricies arcu velit ut diam. Aenean fermentum luctus nulla, eu malesuada urna consequat in. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque massa lacus, sodales id facilisis ac, lobortis sed arcu. Donec hendrerit fringilla ligula, ac rutrum nulla bibendum id. Cras sapien ligula, tincidunt eget euismod nec, ultricies eu augue. Nulla vitae velit sollicitudin magna mattis eleifend. Nam enim justo, vulputate vitae pretium ac, rutrum at turpis. Aenean id magna neque. Sed rhoncus aliquet viverra.
+                        </p>
+                    </div>
+                </section>
+            </section>
+        </div> <!-- end mainContent -->
+            
+		<!------------------------------------------------------------->
+		<!---- SIDEBAR CONTENT ---->
+		<!------------------------------------------------------------->
+		<aside>
+			
+		</aside>
+    </div> <!-- end content -->
+    <footer>
+        <!-- Footer -->
+    </footer>
 
 </div> <!-- end container -->
 
